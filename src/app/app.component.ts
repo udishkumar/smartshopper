89023696d272790f0bf06bcd2134d76bc6e0efc6
udishkumar <udishkum@usc.edu>
2024-09-02 13:50:17 -0700

    API url back to localhost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,10 +61,9 @@ export class AppComponent implements OnInit {
   selectedProduct: any = null;
   wishlistProducts: any[] = [];
   
-  // serverUrl: string = 'http://localhost:8080';
+  serverUrl: string = 'http://localhost:8080';
   // serverUrl: string = 'https://uk-hw3.wl.r.appspot.com';
-
-  serverUrl: string = 'https://smartshoprr.netlify.app';
+  // serverUrl: string = 'https://smartshoprr.netlify.app';
   matchingProduct: any;
   wishlistActive: boolean = false;
   resultSectionActive: boolean = true;
